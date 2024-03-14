ce481d2d8e95ca79280642da114b051d1426e01d
S379467 <163399200+S379467@users.noreply.github.com>
2024-03-14 20:12:27 +1100

    Delete WEEK1.py

--- WEEK1.py
+++
@@ -1,7 +0,0 @@
-x=int(input("Integer Detection Program\nINSERT INT:"))
-if x<0:
-    print("Negative INT detected")
-elif x==0:
-    print("OVERHEATING... Zero != real number")
-else:
-        print("Positive INT detected")
