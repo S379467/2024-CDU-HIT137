6d907385113d70b4c5134b697ebbee567b9874c1
S379467 <163399200+S379467@users.noreply.github.com>
2024-04-04 18:35:57 +1100

    Update Q2 Chapter 1

--- from PIL import Image.py
+++ from PIL import Image.py
@@ -1,10 +1,13 @@
 import numpy as np
 from PIL import Image
-import random
+import time
 
-n = random.randint(0,255)
-print(n)
-#PLACEHOLDER
+current_time = int(time.time())
+generated_number = (current_time % 100) + 50
+if generated_number % 2 == 0:
+    generated_number += 10  
+n = generated_number
+print("RGB values are altered by", n)
 
 with Image.open("chapter1.jpg") as original:
     original.load()
@@ -20,8 +23,7 @@ original_array = np.asarray(original)
 conversion_array = np.asarray(conversion)
 #converts into array so RGB values can be manipulated
 
-out_array = original_array + conversion_array #adds n to all RGB values 
-"""!ISSUE after 255 it cycles back to 0 value"""
+out_array = original_array + conversion_array #adds n to all RGB values, if value > 255 wrap around to 0
 out = Image.fromarray(out_array)
 out.show()
 
@@ -31,8 +33,7 @@ for x in range(0, width):
     for y in range(0, height):
         r, g, b = out.getpixel((x, y))
         r_count = r_count + r
-print("r_count is", r_count)
-        
+print("Sum of all R values in chapter1_out is", r_count)
 
 
-#How to save file??
+out.save("chapter1_out.png")
